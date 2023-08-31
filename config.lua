20714621b8593a14aec5f511bcd5fd44472c39a5
Vannsa <93377560+Vannsa@users.noreply.github.com>
2023-08-31 20:38:33 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config.SharedAccount = true -- true = give the money to a society account Change // Config Line 10 to your Society Account
+Config.Money = 1500 --How much cash it costs to repair the body
+
+Config.BlipName = "Body repair" --Change the Blip Name on the Map
+Config.LSRequired = 1 --How many Mechanics need to be on duty so ppl can't use the repair body shop anymore
+Config.LSJobName = "mechanic" --Job Name that needs to be on duty 
+
+Config.Society = "society_mechanic" --Society where the money is transferred
+
+Config.UsingESXLegacy = false --just leave it as is
---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'Vannsa'
+
+client_scripts {
+    'client.lua',
+}
+server_scripts {
+    'server.lua',
+}
+
+shared_scripts {
+    'config.lua'
+}
+
